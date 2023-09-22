463099705949d2806b1211e908444d947ec1c1f1
semdy <501859275@qq.com>
2023-09-22 13:05:27 +0800

    fisrt commit

---
+++ android/app/src/main/java/com/tvbrowser/BootReceiver.java
@@ -0,0 +1,15 @@
+package com.tvbrowser;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+        context.startActivity(i);
+    }
+}
